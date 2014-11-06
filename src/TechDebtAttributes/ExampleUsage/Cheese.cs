a2a47ad4193a3ac2b71637c03612041f22d20623
Jason Roberts <osjasonroberts@gmail.com>
2014-11-06 12:30:30 +0800

    added some internal class test cases

---
+++ src/TechDebtAttributes/ExampleUsage/Cheese.cs
@@ -0,0 +1,14 @@
+﻿using TechDebtAttributes;
+
+namespace ExampleUsage
+{
+    [TechDebt(3,6, description = "What kind of cheese is this?")]
+    internal class Cheese
+    {
+        [TechDebt(3, 8, description = "What exactly is inner cheese")]
+        internal class InnerCheese
+        {
+            
+        }
+    }
+}
